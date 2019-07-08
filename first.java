58196f005f9b11e4f0a7db9bdaa1cf9264349084
Neeharika-387 <51849507+Neeharika-387@users.noreply.github.com>
2019-07-08 19:31:22 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int x=0;
+	    if(x>0)
+	    {
+	        System.out.println("positive");
+	    }else if(x<0){
+	        System.out.println("negative");
+	    }else{
+	        System.out.println("zero");
+	    }
+	}
+}
